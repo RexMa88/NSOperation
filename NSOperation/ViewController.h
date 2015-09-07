98c009b465f164ab50c4f60985e32102b26f58c2
RexMa88 <rex.smith98@gmail.com>
2015-09-07 09:45:42 +0800

    Project Commit

---
+++ NSOperation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSOperation
+//
+//  Created by Rex Ma on 15/7/28.
+//  Copyright (c) 2015年 Rex Ma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NSOperation/ViewController.m
@@ -0,0 +1,48 @@
+//
+//  ViewController.m
+//  NSOperation
+//
+//  Created by Rex Ma on 15/7/28.
+//  Copyright (c) 2015年 Rex Ma. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+//    NSOperationQueue *mainQueue = [NSOperationQueue mainQueue];
+    
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    
+    NSBlockOperation *operationZero = [NSBlockOperation blockOperationWithBlock:^{
+        NSLog(@"The queue Zero");
+    }];
+    
+    NSBlockOperation *operationOne = [NSBlockOperation blockOperationWithBlock:^{
+        NSLog(@"The queue One");
+    }];
+    
+    NSBlockOperation *operationTwo = [NSBlockOperation blockOperationWithBlock:^{
+        NSLog(@"The queue Two");
+    }];
+    
+    [queue addOperations:@[operationZero, operationOne, operationTwo] waitUntilFinished:YES];
+//    [queue addOperation:operation];
+    
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
